6fb63ab7c60bbc15ab69628dcfe96ab977bc605a
MajidAmiriDev <majidamiri.dev@gmail.com>
2024-09-15 10:06:32 +0330

    test

--- utils/firebase.js
+++
@@ -1,8 +0,0 @@
-const admin = require('firebase-admin');
-//const serviceAccount = require('../../classeh-test-firebase-adminsdk-glwqq-55b0437eb1.json'); // کلید JSON که از Firebase Console دریافت کردید
-
-admin.initializeApp({
-//    credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
