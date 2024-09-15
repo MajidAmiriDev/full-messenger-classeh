fac196bdddb7f9dc91977f30b843c4d67761bf5a
MajidAmiriDev <majidamiri.dev@gmail.com>
2024-09-15 09:39:16 +0330

    add firebase tone

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,8 +1,8 @@
 const admin = require('firebase-admin');
-//const serviceAccount = require('../path/to/your-firebase-adminsdk.json'); // کلید JSON که از Firebase Console دریافت کردید
+const serviceAccount = require('./classeh-test-firebase-adminsdk-glwqq-55b0437eb1.json'); // کلید JSON که از Firebase Console دریافت کردید
 
 admin.initializeApp({
-    //credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
