8b78ea7968ed7e14dfa632a5b7f840af94b377b3
MajidAmiriDev <majidamiri.dev@gmail.com>
2024-09-15 09:51:35 +0330

    add firebase tokne

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./classeh-test-firebase-adminsdk-glwqq-55b0437eb1.json'); // کلید JSON که از Firebase Console دریافت کردید
+const serviceAccount = require('../../classeh-test-firebase-adminsdk-glwqq-55b0437eb1.json'); // کلید JSON که از Firebase Console دریافت کردید
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
